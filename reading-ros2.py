e78c7b02dcc33bea6be83b26a4fa248c9f8fe41a
Samuel Cerezo <108949540+samuelcerezoo@users.noreply.github.com>
2024-10-01 11:44:09 +0200

    Update reading-ros2.py

--- reading-ros2.py
+++ reading-ros2.py
@@ -2,7 +2,12 @@ from rosbags.rosbag2 import Reader
 from rosbags.typesys import Stores, get_typestore
 
 
-rosfile_path = "/home/samuel/Desktop/d435/rosbag-01-oct"
+#rosfile_path = "/home/samuel/Desktop/d435/rosbag-01-oct"
+#search_topic = '/chatter'
+
+
+rosfile_path = "/home/samuel/Desktop/d435/pics_samuel"
+search_topic = '/camera/camera/color/camera_info'
 
 
 # Create a typestore and get the string class.
@@ -11,11 +16,25 @@ typestore = get_typestore(Stores.LATEST)
 # Create reader instance and open for reading.
 with Reader(rosfile_path) as reader:
     # Topic and msgtype information is available on .connections list.
+    counter = 1
     for connection in reader.connections:
-        print("Conection: ", connection.topic, connection.msgtype)
-
+        print("Connection N°", counter, "--> Topic:",connection.topic, "msgtype:",connection.msgtype)
+        counter+=1
+    
+    counter = 0
     # Iterate over messages.
     for connection, timestamp, rawdata in reader.messages():
+        if connection.topic == search_topic:
+            msg = typestore.deserialize_cdr(rawdata, connection.msgtype)
+            print("Message:",msg,"Timestamp:",timestamp)
+        counter+=1
+    
+    print("Total messages: ",counter)
+
+'''
+    # The .messages() method accepts connection filters.
+    connections = [x for x in reader.connections if x.topic == '/imu_raw/Imu']
+    for connection, timestamp, rawdata in reader.messages(connections=connections):
         msg = typestore.deserialize_cdr(rawdata, connection.msgtype)
-        #print(msg.header.frame_id)
-        print(msg.data)
+        print(msg.header.frame_id)
+'''
