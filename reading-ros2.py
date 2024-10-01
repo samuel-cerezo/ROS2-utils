9183ff4f02d7626cad9bd8a9a807fdc4f100f643
Samuel Cerezo <samuel_cerezo@icloud.com>
2024-10-01 10:36:00 +0200

    update

--- reading-ros2.py
+++ reading-ros2.py
@@ -9,16 +9,10 @@ typestore = get_typestore(Stores.LATEST)
 with Reader('/home/samuel/Desktop/d435/rosbag-01-oct') as reader:
     # Topic and msgtype information is available on .connections list.
     for connection in reader.connections:
-        print(connection.topic, connection.msgtype)
+        print("Conection: ", connection.topic, connection.msgtype)
 
     # Iterate over messages.
     for connection, timestamp, rawdata in reader.messages():
-        if connection.topic == '/imu_raw/Imu':
-            msg = typestore.deserialize_cdr(rawdata, connection.msgtype)
-            print(msg.header.frame_id)
-
-    # The .messages() method accepts connection filters.
-    connections = [x for x in reader.connections if x.topic == '/imu_raw/Imu']
-    for connection, timestamp, rawdata in reader.messages(connections=connections):
         msg = typestore.deserialize_cdr(rawdata, connection.msgtype)
-        print(msg.header.frame_id)
+        #print(msg.header.frame_id)
+        print(msg.data)
