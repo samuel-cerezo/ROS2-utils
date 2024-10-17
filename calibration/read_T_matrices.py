0e23da8b030c26e78e030df8c1b6427ef54103d3
Samuel Adrian Cerezo <samuel_cerezo@icloud.com>
2024-10-17 17:45:24 +0200

    update

--- calibration/read_T_matrices.py
+++ calibration/read_T_matrices.py
@@ -1,5 +1,30 @@
 import numpy as np
-from include.posesYAML import extract_transformation_matrices  # Import custom functions
+import yaml
+import os
+
+def extract_transformation_matrices(yaml_file):
+    # Load the YAML file
+    with open(yaml_file, 'r') as file:
+        data = yaml.safe_load(file)
+
+    # Extract transformations and save them in variables
+    transformations = data.get('T', [])
+
+    # Dictionary to hold matrices with variable names
+    T_matrices = {}
+
+    for transformation in transformations:
+        source_frame = transformation['source_frame']
+        destination_frame = transformation['destination_frame']
+        matrix_data = transformation['matrix']['data']
+        
+        # Create a unique variable name based on source and destination frames
+        variable_name = f"{source_frame}_to_{destination_frame}".replace('-', '_')  # Replace any dashes with underscores
+        matrix_array = np.array(matrix_data).reshape(4, 4)
+
+        T_matrices[variable_name] = matrix_array
+
+    return T_matrices #return a dictionary. Every T is save with a key-value correspondence
 
 def main():
     # Path to the cleaned YAML file
