f34439e308ab12091e0b5b3e11fc48dba96e58e1
Samuel Adrian Cerezo <samuel_cerezo@icloud.com>
2024-10-17 17:51:23 +0200

    documentation added

--- read_T_matrices.py
+++ read_T_matrices.py
@@ -2,7 +2,39 @@ import numpy as np
 import yaml
 import os
 
+import yaml
+import numpy as np
+
 def extract_transformation_matrices(yaml_file):
+    """
+    Extract transformation matrices from a YAML file.
+
+    This function reads a YAML file that contains transformation matrices
+    between different frames (e.g., camera, robot, etc.) and returns 
+    these matrices as a dictionary where the keys are formatted as 
+    'source_frame_to_destination_frame'.
+
+    Args:
+        yaml_file (str): The path to the YAML file containing transformation matrices.
+        
+    Returns:
+        dict: A dictionary containing transformation matrices. The keys are formatted 
+              as 'source_frame_to_destination_frame', and the values are 4x4 transformation 
+              matrices represented as NumPy arrays.
+    
+    Raises:
+        FileNotFoundError: If the specified YAML file does not exist.
+        yaml.YAMLError: If the YAML file cannot be parsed or is invalid.
+    
+    Example:
+        >>> matrices = extract_transformation_matrices('transformations.yaml')
+        >>> print(matrices['color_optical_to_robot_flange'])
+        [[ 0.0202555  0.999719   0.0122748 -0.00040487]
+         [-0.99961    0.0200144  0.0194587 32.9645   ]
+         [ 0.0192076 -0.0126642  0.999735  44.6762   ]
+         [ 0.         0.         0.         1.       ]]
+    """
+
     # Load the YAML file
     with open(yaml_file, 'r') as file:
         data = yaml.safe_load(file)
@@ -24,7 +56,8 @@ def extract_transformation_matrices(yaml_file):
 
         T_matrices[variable_name] = matrix_array
 
-    return T_matrices #return a dictionary. Every T is save with a key-value correspondence
+    return T_matrices  # Return a dictionary. Every transformation matrix is saved with a key-value correspondence.
+
 
 def main():
     # Path to the cleaned YAML file
