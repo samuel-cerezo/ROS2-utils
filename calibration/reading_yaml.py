e79fb6f7d0f74518e42dd8175515a163ca558ea5
Samuel Cerezo <samuel_cerezo@icloud.com>
2024-10-17 16:00:27 +0200

    update

--- calibration/reading_yaml.py
+++ calibration/reading_yaml.py
@@ -1,5 +1,7 @@
 import yaml
 import numpy as np
+import os
+import numpy as np
 
 def read_yaml(file_path):
     with open(file_path, 'r') as file:
@@ -10,16 +12,16 @@ def extract_transformation_matrices(data):
     trans_matrices = []
     
     for item in data['T']:
-        parent_frame = item['parent_frame']
-        child_frame = item['child_frame']
+        source_frame = item['source_frame']
+        destination_frame = item['destination_frame']
         matrix_data = item['matrix']['data']
         
         # Reshape the flat list into a 4x4 matrix
         matrix = np.array(matrix_data).reshape(4, 4)
         
         trans_matrices.append({
-            'parent_frame': parent_frame,
-            'child_frame': child_frame,
+            'source_frame': source_frame,
+            'destination_frame': destination_frame,
             'matrix': matrix
         })
     
@@ -27,8 +29,9 @@ def extract_transformation_matrices(data):
 
 def main():
     # Path to the cleaned YAML file
-    yaml_file = 'calibration/transformations.yaml'
-    
+    yaml_file = '/home/samuel/dev/environment_modeling/scripts/KUKA/calibration/transformations.yaml'
+    current_directory = os.getcwd()
+
     # Read the YAML file
     data = read_yaml(yaml_file)
     
@@ -37,8 +40,9 @@ def main():
     
     # Example usage: printing the matrices
     for T in transformation_matrices:
-        print(f"Transformation from {T['parent_frame']} to {T['child_frame']}:")
+        print(f"Transformation from {T['source_frame']} to {T['destination_frame']}:")
         print(T['matrix'], "\n")
 
 if __name__ == "__main__":
+
     main()
