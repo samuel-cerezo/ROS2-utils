9db6d21ae281f75e1e39b9fb608c59c5e7c2dc4d
Samuel Adrian Cerezo <samuel_cerezo@icloud.com>
2025-03-06 14:26:22 +0100

    .

--- post_processing/obtaining_trajectories.py
+++ post_processing/obtaining_trajectories.py
@@ -5,6 +5,23 @@ from scipy.spatial.transform import Rotation as R
 import os
 import argparse
 
+def compute_ate_rmse(estimated_positions, groundtruth_positions):   
+    """
+    Calcula el Absolute Trajectory Error (ATE) basado en RMSE.
+
+    Parámetros:
+    - estimated_positions: np.array de forma (N,3) con posiciones estimadas (X, Y, Z)
+    - groundtruth_positions: np.array de forma (N,3) con posiciones ground truth (X, Y, Z)
+
+    Retorna:
+    - ATE RMSE 
+    """
+    assert estimated_positions.shape == groundtruth_positions.shape, "Las trayectorias deben tener la misma longitud"
+
+    errors = np.linalg.norm(estimated_positions - groundtruth_positions, axis=1)
+    ate_rmse = np.sqrt(np.mean(errors**2))
+    
+    return ate_rmse
 
 def load_yaml_transformations(yaml_path):
     with open(yaml_path, 'r') as f:
@@ -40,11 +57,19 @@ def pose_to_homogeneous(q, t):
     T[:3, 3] = t
     return T
 
-def transform_poses(poses, transform):
+def transform_poses_left(poses, transform):
     transformed_poses = []
     for q, t in poses:
         T = pose_to_homogeneous(q, t)
         T_transformed = transform @ T
+        transformed_poses.append(T_transformed[:3, 3])
+    return np.array(transformed_poses)
+
+def transform_poses_right(poses, transform):
+    transformed_poses = []
+    for q, t in poses:
+        T = pose_to_homogeneous(q, t)
+        T_transformed = T @ transform
         transformed_poses.append(T_transformed[:3, 3])
     return np.array(transformed_poses)
 
@@ -57,37 +82,37 @@ def main():
     
     #dataset_path = os.path.join('/home/samuel/dev/environment_modeling/ROSBAGS', args.input)
     dataset_path = os.path.join('/Volumes/SSD/archivos/KUKA_dev/environment_modeling/ROSBAGS', args.input)
-
-    # Cargar transformaciones
-    yaml_path = "extrinsics.yaml"
-
-
-    transforms = load_yaml_transformations(os.path.join(dataset_path, yaml_path))
+    transforms = load_yaml_transformations(os.path.join(dataset_path,  "extrinsics.yaml"))
 
     # Matriz de transformación completa de flange a world
-    T_world_base_marker = transforms[('base_marker_ring', 'world')]     # base markers --> world
-    T_base_marker_robot_base = transforms[('base_marker_ring', 'robot_base')]   # base markers --> robot base
-    T_flange_marker_robot_flange = transforms[('flange_marker_ring', 'robot_flange')]   # flange markers --> flange 
-    T_rgb_robot_flange = transforms[('rgb_sensor', 'robot_flange')]  # camara kirill --> flange
-
-    T_world_robot_base = T_world_base_marker @ np.linalg.inv(T_base_marker_robot_base)
-    T_world_robot_flange = T_world_robot_base @ np.linalg.inv(T_flange_marker_robot_flange)
-    T_world_camera = T_world_robot_flange @ np.linalg.inv(T_rgb_robot_flange)
+    T_world_base_marker = transforms[('base_marker_ring', 'world')]                     # base markers --> world
+    T_robot_base_base_marker = transforms[('base_marker_ring', 'robot_base')]           # base markers --> robot base
+    T_robot_flange_flange_marker = transforms[('flange_marker_ring', 'robot_flange')]   # flange markers --> flange 
+    T_robot_flange_rgb = transforms[('rgb_sensor', 'robot_flange')]                    # camera  --> flange
+    T_flange_marker_robot_flange = np.linalg.inv(T_robot_flange_flange_marker)          # flange markers --> flange
+    T_rgb_flange_markers = np.linalg.inv(T_robot_flange_rgb) @ np.linalg.inv(T_flange_marker_robot_flange)  # flange markers --> camera 
+    
+    #print(T_robot_flange_rgb)
+    print(T_flange_marker_robot_flange)
+    print(np.linalg.inv(T_robot_flange_rgb))
+    T_world_robot_base = T_world_base_marker @ np.linalg.inv(T_robot_base_base_marker)  # robot base --> world
+    #T_world_robot_flange = T_world_robot_base @ np.linalg.inv(T_flange_marker_robot_flange)
 
     # Cargar poses
     timestamps_flange, poses_flange = load_poses(os.path.join(dataset_path, "flange_poses_aligned.txt"))
     timestamps_gt, poses_gt = load_poses(os.path.join(dataset_path, "groundtruth_aligned.txt"))
-    # aca hay que cambiar, porq las poses son respecto del anillo, y necesito transformar hacia el sistema de coordenada de camara 
-    # Transformar poses al sistema world
-    flange_world_positions = transform_poses(poses_flange, T_world_robot_flange)
-    camera_world_positions = transform_poses(poses_flange, T_world_camera)
-    groundtruth_positions = np.array([t for _, t in poses_gt])
+    flange_world_positions = transform_poses_left(poses_flange, T_world_robot_base)
+    groundtruth_positions = transform_poses_right(poses_gt, T_flange_marker_robot_flange)
+
+    # Suponiendo que flange_world_positions y groundtruth_positions son arrays (N,3)
+    ate_rmse = compute_ate_rmse(flange_world_positions, groundtruth_positions)
+    print(f"ATE RMSE: {ate_rmse:.4f} mm: \nmeans that, on average, the absolute distance between the estimated trajectory and the ground truth trajectory is {ate_rmse:.4f} mm.")
+    #groundtruth_marker_positions = np.array([t for _, t in poses_gt])
 
     # Graficar
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     ax.plot(flange_world_positions[:, 0], flange_world_positions[:, 1], flange_world_positions[:, 2], label="Flange (World)", linestyle='--')
-    ax.plot(camera_world_positions[:, 0], camera_world_positions[:, 1], camera_world_positions[:, 2], label="Camera (World)", linestyle='-.')
     ax.plot(groundtruth_positions[:, 0], groundtruth_positions[:, 1], groundtruth_positions[:, 2], label="Ground Truth", linestyle=':')
     ax.legend()
     plt.show()
