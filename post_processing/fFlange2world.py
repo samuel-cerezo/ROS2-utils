7383624b72cebf99a13dd52d3d7fa7ec7e8c9d66
Samuel Adrian Cerezo <samuel_cerezo@icloud.com>
2025-03-13 20:40:09 +0100

    last update

--- post_processing/fFlange2world.py
+++ post_processing/fFlange2world.py
@@ -6,16 +6,19 @@ import os
 import argparse
 from transformations_utils import *
 
-
 def main():
+    # pointing to the dataset folder
     parser = argparse.ArgumentParser(description='Compare poses (obtained by joint-robot vs gt)')
-    parser.add_argument('--input', type=str, required=True, help='Folder containing dataset')
+    parser.add_argument('--path', type=str, required=True, help='Folder containing dataset')
     args = parser.parse_args()
-    
-    dataset_path = os.path.join('/Volumes/SSD/archivos/KUKA_dev/environment_modeling/ROSBAGS', args.input)
-    transforms = load_yaml_transformations(os.path.join(dataset_path,  "extrinsics.yaml"))
+        
+    dataset_path = os.path.join(args.path)
+    # we retrieve the data from the files
+    timestamps_flange, poses_flange = load_poses(os.path.join(dataset_path, "robot_data/flange_poses.txt")) # flange wrt base robot
+    timestamps_gt, poses_gt = load_poses(os.path.join(dataset_path, "groundtruth.txt"))    # flange-markers wrt world
+    transforms = load_yaml_transformations(os.path.join(dataset_path,  "extrinsics.yaml"))  # to obtain the transform matrixs
 
-    #----- retrieving all transformations ----
+    #----- retrieving all transformations (T_target_source) -------
     T_world_base_marker = transforms[('base_marker_ring', 'world')]                     # base markers --> world
     T_robot_base_base_marker = transforms[('base_marker_ring', 'robot_base')]           # base markers --> robot base
     T_robot_flange_flange_marker = transforms[('flange_marker_ring', 'robot_flange')]   # flange markers --> flange 
@@ -24,21 +27,14 @@ def main():
     # ---- calculation for useful transformations ---
     T_flange_marker_robot_flange = np.linalg.inv(T_robot_flange_flange_marker)          # flange  --> flange markers
     T_rgb_flange_markers = np.linalg.inv(T_robot_flange_rgb) @ np.linalg.inv(T_flange_marker_robot_flange)  # flange markers --> camera 
-    T_base_marker_robot =  np.linalg.inv(T_robot_base_base_marker)
-    T_world_robot_base = T_world_base_marker @ T_base_marker_robot                      # robot base --> world
+    T_base_marker_robot_base =  np.linalg.inv(T_robot_base_base_marker)                      # robot base --> robot marker
+    T_world_robot_base = T_world_base_marker @ T_base_marker_robot_base                      # robot base --> world
 
-    timestamps_flange, poses_flange = load_poses(os.path.join(dataset_path, "robot_data/flange_poses.txt"))
-    timestamps_gt, poses_gt = load_poses(os.path.join(dataset_path, "groundtruth.txt"))
-
-    camera_world = []
-    camera_world_gt = []
-
+    # we convert the data into a same world reference frame
     camera_world_q, camera_world_positions  = compute_camera_world_positions(poses_flange, T_world_base_marker, T_robot_base_base_marker, T_robot_flange_rgb)
     camera_world_q_gt, camera_world_positions_gt = compute_camera_world_positions_gt(poses_gt, T_rgb_flange_markers)
 
-    print(len(camera_world_positions_gt))
-    print(len(camera_world_positions))
-    # Graficar
+    # plotting
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     ax.plot(camera_world_positions[:, 0], camera_world_positions[:, 1], camera_world_positions[:, 2], label="Camera (obtained by robot-joints)", linestyle='--')
@@ -49,9 +45,5 @@ def main():
 if __name__ == '__main__':
     main()
 
-
-
-
-
-
-
+# example:
+#           python3 fFlange2world.py --path /Volumes/SSD/archivos/KUKA_dev/environment_modeling/ROSBAGS/1-dark-tr
